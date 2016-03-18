0b3de590d7faf3c25b34170800c063fbccea1d2d
salemdar <sa.alemdar@hotmail.com>
2016-03-18 14:50:11 +0200

    travis experiments

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
+echo "samet"
 echo "$TRAVIS_BRANCH"
-echo $TRAVIS_BRANCH
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_REPO_SLUG" = "salemdar/angular2-cookie" ]; then
-#  npm publish ./dist --tag next --access public
-fi
+#if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_REPO_SLUG" = "salemdar/angular2-cookie" ]; then
+##  npm publish ./dist --tag next --access public
+#fi
