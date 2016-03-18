df80e19e7b6fec1cd017a6547d487465582bbc45
salemdar <sa.alemdar@hotmail.com>
2016-03-18 15:10:46 +0200

    version bump: 1.0.6

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -1,7 +1,5 @@
-#!/usr/bin/env bash
-
-echo "samet"
-echo "$TRAVIS_BRANCH"
-#if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_REPO_SLUG" = "salemdar/angular2-cookie" ]; then
-##  npm publish ./dist --tag next --access public
-#fi
+#!/bin/bash
+set -ev
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_REPO_SLUG" = "salemdar/angular2-cookies" ]; then
+#  npm publish ./dist --tag next --access public
+fi
